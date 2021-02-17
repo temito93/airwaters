a9b494bc5d0fc5537133218610b3d77d27809283
Temo Jincharadze <temotemo161@gmail.com>
2021-02-17 19:14:10 +0400

    updated contact section margin

--- scroll-list-bottom-l.js
+++ scroll-list-bottom-l.js
@@ -2,7 +2,7 @@ listTHREE = document.getElementById("listTHREE");
 
 var myScrollFuncfive = function() {
   var y = window.scrollY;
-  if (y >= 500) {
+  if (y >= 450) {
     listTHREE.className = "list-three show-box-l-bottom"
   } else {
     listTHREE.className = "list-three hide-box-l-bottom"
--- scroll-list-t.js
+++ scroll-list-t.js
@@ -2,7 +2,7 @@ listONE = document.getElementById("listONE");
 
 var myScrollFuncthree = function() {
   var y = window.scrollY;
-  if (y >= 500) {
+  if (y >= 450) {
     listONE.className = "list-one show-box-l"
   } else {
     listONE.className = "list-one hide-box-l"
